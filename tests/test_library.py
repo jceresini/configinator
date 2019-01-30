bf83ad2ceb678a1eef63e46a8ff0baa1096b5b14
Joe Ceresini <joe.ceresini@cleardata.com>
2019-01-30 14:07:33 -0500

    Update tests

--- tests/test_library.py
+++ tests/test_library.py
@@ -9,7 +9,11 @@ test_config_data = {
     'some_username': 'admin',
     'some_password': 'foofoo123',
     'b64:some_secret': 'dGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZwo=',
-    'nope:some_thing': 'qwerty32'
+    'nope:some_thing': 'qwerty32',
+    'nested': {
+        'item': 'data',
+        'item2': 'moardata'
+    }
 }
 
 test_config_json = json.dumps(test_config_data)
@@ -17,8 +21,18 @@ test_config_json = json.dumps(test_config_data)
 def test_create_from_dict():
     config = Config(test_config_data)
     assert config.some_username == 'admin'
-    assert config.some_fake_item == None
     assert 'brown fox' in config.some_secret
+
+def test_nested_data():
+    config = Config(test_config_data)
+    assert config.nested.__class__ == Config
+    assert config.nested.item == 'data'
+
+def test_bad_attribute():
+    config = Config(test_config_data)
+
+    with pytest.raises(AttributeError):
+        config.fake_item
 
 def test_use_as_dict():
     config = Config(test_config_data)
@@ -31,7 +45,6 @@ def test_load_from_file(m):
 
     config = Config.load_from_file('fake_name')
     assert config.some_username == 'admin'
-    assert config.some_fake_item == None
     assert 'brown fox' in config.some_secret
 
 @patch("builtins.open", create=True)
